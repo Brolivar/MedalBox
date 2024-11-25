4a0bbbc297698fdef4b83aeb95d1b1c53fc26ebe
jebolivar <brolivars@gmail.com>
2024-11-26 00:20:14 +0100

    Initial Commit

---
+++ MedalBoxTests/MedalBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedalBoxTests.swift
+//  MedalBoxTests
+//
+//  Created by Jose Bolivar on 26/11/24.
+//
+
+import Testing
+@testable import MedalBox
+
+struct MedalBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
